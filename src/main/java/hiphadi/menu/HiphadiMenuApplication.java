efc784e8053f31a8dbbf7151ba2fd56671689b1a
NaMinhyeok <nmh9097@gmail.com>
2024-10-29 22:23:09 +0900

    chore : initial commit

---
+++ src/main/java/hiphadi/menu/HiphadiMenuApplication.java
@@ -0,0 +1,13 @@
+package hiphadi.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiphadiMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiphadiMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hiphadi/menu/HiphadiMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package hiphadi.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiphadiMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
