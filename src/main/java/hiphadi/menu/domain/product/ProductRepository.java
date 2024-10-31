f317602514c5b88d0056e41d63f671fffca1177c
EunHyunsu <ehs208@naver.com>
2024-10-31 20:37:20 +0900

    refactor: Param 어노테이션 제거

--- src/main/java/hiphadi/menu/domain/product/ProductRepository.java
+++ src/main/java/hiphadi/menu/domain/product/ProductRepository.java
@@ -1,12 +1,10 @@
 package hiphadi.menu.domain.product;
 
-
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,15 +12,15 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	List<Product> findAllByOrderByCategoryDescIdDesc(Pageable pageable);
 
 	@Query("""
-    SELECT p FROM Product p
-    WHERE 
-        p.category < :categoryCursor 
-        OR (p.category = :categoryCursor AND p.id < :idCursor)
-    ORDER BY 
-        p.category DESC, 
-        p.id DESC
-    """)
-	List<Product> findByCursor(@Param("categoryCursor") String categoryCursor,
-		@Param("idCursor") Long idCursor,
+		SELECT p FROM Product p
+		WHERE 
+		    p.category < :categoryCursor 
+		    OR (p.category = :categoryCursor AND p.id < :idCursor)
+		ORDER BY 
+		    p.category DESC, 
+		    p.id DESC
+		""")
+	List<Product> findByCursor(String categoryCursor,
+		Long idCursor,
 		Pageable pageable);
 }
