343cb5cb338aa6171580f0082dd5cc65e4acf552
EunHyunsu <ehs208@naver.com>
2024-12-13 19:06:01 +0900

    feat: 활성화 메뉴 테이블 구현

---
+++ src/main/java/hiphadi/menu/domain/currentSituation/CurrentSituation.java
@@ -0,0 +1,55 @@
+package hiphadi.menu.domain.currentSituation;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import hiphadi.menu.domain.menuAvailability.Situation;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CurrentSituation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private Situation situation;  // PARTY, NORMAL
+
+	@NotNull
+	private boolean isActive;    // true/false
+
+
+	private void activate() {
+		this.isActive = true;
+	}
+
+	private void deactivate() {
+		this.isActive = false;
+	}
+
+	// 정적 팩토리 메서드
+	public static CurrentSituation changeSituationStatus(CurrentSituation situationType, boolean active) {
+		if (active) {
+			situationType.activate();
+		} else {
+			situationType.deactivate();
+		}
+		return situationType;
+	}
+
+}
+
